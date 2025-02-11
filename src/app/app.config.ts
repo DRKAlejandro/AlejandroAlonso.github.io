57b8b2d344509804564e195752e43dc25cf43509
DRKAlejandro <jesalehalo@gmail.com>
2025-02-11 15:04:11 -0600

    header-footer-home

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"personalweb-9235b","appId":"1:967516979851:web:cac5914ca21e79b8e8ca8e","storageBucket":"personalweb-9235b.firebasestorage.app","apiKey":"AIzaSyAaRTZFmJ_XEmL4Z2tPORrERKh6ukM3oNs","authDomain":"personalweb-9235b.firebaseapp.com","messagingSenderId":"967516979851","measurementId":"G-SNZY07VS3C"})), provideAnalytics(() => getAnalytics()), ScreenTrackingService, provideFirestore(() => getFirestore())]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyAaRTZFmJ_XEmL4Z2tPORrERKh6ukM3oNs",
+        authDomain: "personalweb-9235b.firebaseapp.com",
+        projectId: "personalweb-9235b",
+        storageBucket: "personalweb-9235b.firebasestorage.app",
+        messagingSenderId: "967516979851",
+        appId: "1:967516979851:web:cac5914ca21e79b8e8ca8e",
+        measurementId: "G-SNZY07VS3C"
+    }
+};
